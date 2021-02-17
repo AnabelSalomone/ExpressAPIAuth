db89ec4c6f0a9433ac20b21be96b3c4f80a2c8a7
AnabelSalomone <anabel.salomone@gmail.com>
2021-02-17 20:43:10 +0100

    added validation to schema

--- model/User.js
+++ model/User.js
@@ -1,27 +1,32 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        min: 6
-    },
-    email: {
-        type: String,
-        required: true,
-        max: 255,
-        min:6
-    },
-    password: {
-        type:String,
-        required: true,
-        max: 1024,
-        min:6
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: [6, "ERROR"],
+    maxlength: [25, "ERROR"]
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 255,
+    minlength: [6, "ERROR"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 1024,
+    minlength: [6, "ERROR"],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model('User', userSchema)

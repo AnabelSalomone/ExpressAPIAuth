cdd755d3cfcd84e77a5e8b8d78fbc00a8200ec6b
AnabelSalomone <anabel.salomone@gmail.com>
2021-02-25 20:00:10 +0100

    added authentication with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   // Check if user already exists
@@ -24,6 +25,19 @@ router.post("/register", async (req, res) => {
     console.log("ERRORRRRRR ", err);
     res.status(400).send(err);
   }
+});
+
+router.post("/login", async (req, res) => {
+  // Check if user already exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Wrong email or password");
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Wrong email or password");
+
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token)
+  res.send("Logged in")
 });
 
 module.exports = router;
